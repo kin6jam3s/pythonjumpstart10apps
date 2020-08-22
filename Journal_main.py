825d874b3f78cce93916af871245c534b0554854
lebronJames <kin6jam3s@gmail.com>
2020-08-22 21:44:45 +0800

    App 4 : Journal App

--- Journal_main.py
+++ Journal_main.py
@@ -5,42 +5,51 @@ This is the Journal_main module.
 import os
 
 
+# Function to load the file 'default' which is the value of journal_name and equal to (name)
 def load(name):
     """
-    This methos creates and loads a new jornal
+    This method creates and loads a new journal
 
     :param name: This base name of journal to load.
     :return: A new Journal data structure populated with file data.
     """
-    data = []
+    data = []  # Initial value of data. Empty list
+    # (name) which is equal o 'default' and sent as a variable on function get_full_pathname
     filename = get_full_pathname(name)
 
-    if os.path.exists(filename):
-        with open(filename) as fin:
-            for entry in fin.readlines():
-                data.append(entry.rstrip())
+    if os.path.exists(filename):  # check if the filename
+        with open(filename) as fin:  # with open - open and close the file
+            for entry in fin.readlines():  # loop and read the lines
+                data.append(entry.rstrip())  # append data on the file for each line - remove white space at the end
 
-    return data
+    return data  # return the data
 
 
+# name = journal_name ; journal_data = journal_data
 def save(name, journal_data):
+    # Get pull path of name(journal_name) using get_full_pathname function
     filename = get_full_pathname(name)
     # print(os.path.abspath(filename)) # will show working folder path
     print('....saving to: {}'.format(filename))
 
+    # opening file(default.jrl) and write all the loaded entry and new entry
     with open(filename, 'w') as fout:  # context manager for cleanup and flash buffer
-        for entry in journal_data:
-            fout.write(entry + '\n')
+        for entry in journal_data:  # go through each line of journal_data
+            fout.write(entry + '\n')  # write each line and separate by newline
 
     # fout = open(filename, 'w')
     # fout.close()
 
 
+# Function for getting the full path or location of the file 'default'
 def get_full_pathname(name):
+    # os.path.abspath = /Users/ronelordonio/dciv2-code/mypython/pythonjumpstart10apps
+    # os.path.join = ./journals/default.jrl
     filename = os.path.abspath(os.path.join('.', 'journals', name + '.jrl'))
-    return filename
+    return filename  # return /Users/ronelordonio/dciv2-code/mypython/pythonjumpstart10apps/journals/default.jrl
 
 
 # text = text ; journal_data = data
 def add_entry(text, journal_data):
+    # This function is only for appending the txt to the journal_data
     journal_data.append(text)
--- Personal_Journal.py
+++ Personal_Journal.py
@@ -1,48 +1,68 @@
 import Journal_main
+
+
 # from Journal_main import load, save
 # from Journal_main import *
+
 
 def main():
     print_header()
     run_event_loop()
 
 
+# Function for showing the header
 def print_header():
     print('-----------------------')
     print(' Journal App')
     print('-----------------------')
 
 
+# Function for getting input from user and event loop while x is not press
 def run_event_loop():
+    # Show the message 'What do you want to do with your journal?'
     print('What do you want to do with your journal?')
+    # user cmd initial value is 'EMPTY' which is not equals to 'x'
     cmd = 'EMPTY'
+    # Journal file name
     journal_name = 'default'
-    journal_data = Journal_main.load(journal_name)  # [] # list ()
+    # Journal data which is imported fom Journal_main and use the function 'load'
+    # journal_name will be the variable to be used on function load
+    journal_data = Journal_main.load(journal_name)  # [] # list () # this is the return data from load function
 
+    # Look to get user input while cmd != x and cmd is blank/empty str
     while cmd != 'x' and cmd:
-        cmd = input('[L]ist entries, [A]dd an entry, E[x]it:  ')
-        cmd = cmd.lower().strip()
+        cmd = input('[L]ist entries, [A]dd an entry, E[x]it:  ')  # user input
+        cmd = cmd.lower().strip()  # str input converted to lower case
         if cmd == 'l':
+            # Function for listing sending journal_data as data
             list_entries(journal_data)
+            # Function for adding sending journal_data as data
         elif cmd == 'a':
             add_entry(journal_data)
+            # If user input 'x' or blank str it will close and save the program
         elif cmd != 'x' and cmd:
             print("Sorry, We dont understant '{}'.".format(cmd))
 
     print('Done, Goodbye!')
+    # sending variable journal_name and journal_data to function named save from Journal_main
     Journal_main.save(journal_name, journal_data)
 
 
 # data = journal_data
 def list_entries(data):
+    # data save to variable entries in reverse
     entries = reversed(data)
-    for idx,i in enumerate(entries):
-        print(idx+1,i)
+    # Get index number using enumerate and entry on reverse data
+    for idx, i in enumerate(entries):
+        print(idx + 1, i)
     # print(data)
 
 
+# Function for adding an entry data = journal_data
 def add_entry(data):
+    # user input to add any string
     text = input('Type your entry, <enter> to exit: ')
+    # data and text will be use as variable for add_entry function on Journal_main program/module
     Journal_main.add_entry(text, data)
     # data.append(text)
 
