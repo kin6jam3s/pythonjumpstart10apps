cefda50e9eb5332bba49e52b64f3e39b78482773
lebronJames <kin6jam3s@gmail.com>
2020-09-14 19:02:43 +0800

    APP 7 : WizGame

--- WizGame_Characters.py
+++ WizGame_Characters.py
@@ -1,13 +1,15 @@
 import random
 
 
-class Creature:
+class Creature:  # This class will be the base for all creature for variable 'name' and 'level'
     # Magic method -
     def __init__(self, name, level):
         self.name = name
         self.level = level
 
+    # This will convert <WizGame_Characters.SmallAnimal object at 0x100d32520> to [Creature: Toad of level 1]
     def __repr__(self):
+        # This will show
         return "Creature: {} of level {}".format(
             self.name, self.level
         )
@@ -28,21 +30,24 @@ class Wizard(Creature):  # For class name should start with CAPITAL letter
     #         self.name, self.level
     #     )
 
+    # Class Creature was called on Wizard class
     def attack(self, active_creature):
-        # print(type(active_creature), active_creature)
+        print(type(active_creature), active_creature.name, active_creature.level,
+              active_creature)  # <class 'WizGame_Characters.SmallAnimal'> Creature: Toad of level 1
         print('----------FIGHT-------------')
         print('The Wizard {} attacks {}!'.format(
             self.name, active_creature.name
         ))
 
         # red_roll = random.randint(1, 12) * self.level
-        red_roll = self.get_defensive_roll()
+        red_roll = self.get_defensive_roll()  # using function under class Creature | self for Wizard
         # creature_roll = random.randint(1, 12) * active_creature.level
-        creature_roll = active_creature.get_defensive_roll()
+        creature_roll = active_creature.get_defensive_roll()  # # using function under class Creature
 
         print("Red's Power is : {} !!!".format(red_roll))
         print("Enemy {}'s power is : {}".format(active_creature.name, creature_roll))
 
+        # compare the roll and determine the winner
         if red_roll >= creature_roll:
             print('Red won against enemy {}!!!'.format(active_creature.name))
             return True
@@ -51,28 +56,35 @@ class Wizard(Creature):  # For class name should start with CAPITAL letter
             return False
 
 
+# This will be use base on the active_creature, if class creature is SmallAnimal
+# then it will use this get_defensive_roll
 class SmallAnimal(Creature):
     def get_defensive_roll(self):
         base_roll = super().get_defensive_roll()
+        # print('----', base_roll / 2, self.name)
         return base_roll / 2
 
 
+# Creature class imported
 class Dragon(Creature):
-    def __init__(self, name, level, scaliness, breath_fire):
-        super().__init__(name, level)
+    #  Magic method
+    def __init__(self, name, level, scaliness, breath_fire):  # initialise function variable
+        super().__init__(name, level)  # declare that it will use Creature parameter for name and level
         self.breath_fire = breath_fire
         self.scaliness = scaliness
 
     def get_defensive_roll(self):
-        base_roll = super().get_defensive_roll()
+        base_roll = super().get_defensive_roll()  # use get_defensive_roll() function under Creature class
         # fire_modifier = None
         # if self.breath_fire:
         #     fire_modifier = 5
         # else:
         #     fire_modifier = 1
-
+        # --> Dragon('Dragon', 50, 75, True)
+        # fire modifier is 5 if True and 1 for False
         # fire_modifier = VALUE_IF_TRUE if SOME TEST else VALUE_IF_FALSE
         fire_modifier = 5 if self.breath_fire else 1
+        # print(self.scaliness) - 75
         scale_modifier = self.scaliness / 10
 
         return base_roll * fire_modifier * scale_modifier
--- Wizard_app.py
+++ Wizard_app.py
@@ -6,16 +6,20 @@ from WizGame_Characters import Creature, SmallAnimal, Dragon
 
 
 def main():
+    # calling function for printing header
     print_header()
+    # calling the function for game loop
     game_loop()
 
 
+# Function for creating the header
 def print_header():
     print('--------------------')
     print('    Wizard Game')
     print('--------------------')
 
 
+#
 def game_loop():
     # creatures = [
     #     Creature('Toad', 1),
@@ -38,36 +42,41 @@ def game_loop():
     hero = WizGame_Characters.Wizard('Red Red', 75)
 
     # print(hero)
-
+    # This is for initializing the value of cmd | should be not empty
     cmd = 'EMPTY'
+    # Loop statement using while cmd not equal to 'x'
     while cmd != 'x':
-
         # print(type(creatures))    <class 'list'>
-
+        # selecting creature using random module using choice option
         active_creature = random.choice(creatures)
         # print('--->', active_creature)
         # print(type(active_creature))  # <class 'WizGame_Characters.Creature'>
         print('A {} of level {} has appear from a dark foggy forest...'.format(
             active_creature.name, active_creature.level
         ))
-
+        # User input to the action to perform
         cmd = input('Do [a]ttack, [r]unaway, or [l]ookaround: ')
+        # if statement when cmd equals to 'a' - str converted to lower and strip space
         if 'a' == cmd.lower().strip():
             # print('Attack')
             # Send active_creature variable to attack module under class Wizard
-            if hero.attack(active_creature):
+            if hero.attack(active_creature):  # if return is True proceed to remove that random creature on the list
                 creatures.remove(active_creature)
-            else:
+            else:  # if False
                 print('Red runs back to his Mommy ADANA to eat Sinigang to recover...')
-                time.sleep(5.1)
+                time.sleep(5.1)  # import time module and use option sleep for pause
                 print('Bonjing Red recovered!!!')
-        elif 'r' == cmd.lower().strip():
+        # if statement when cmd equals to 'r' - str converted to lower and strip space
+        elif 'r' == cmd.lower().strip():  # run away and go back to start selecting a new random creature
             print('Bonjing {} Cries and RunAway'.format(hero.name))
+        # if statement when cmd equals to 'l' - str converted to lower and strip space
         elif 'l' == cmd.lower().strip():
             # print('Lookaround')
             print('The Bonjing {} Scans the surrounding and sees'.format(hero.name))
             for i in creatures:
+                # print('---->',type(creatures), creatures) - <class 'list'> [Creature: Toad of level 1 ..]
                 print('* A {} of level {}'.format(i.name, i.level))
+        # if statement when cmd not equal to 'a','l' or 'x'
         elif cmd != 'x' and cmd:
             print('Input not valid.. Try again')
             # break  <- This could be an alternate option for pressing 'x'
